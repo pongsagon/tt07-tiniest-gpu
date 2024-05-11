32afcd5b9c25d1891f390d35690417a534cf6309
pongsagon <mattdsik@hotmail.com>
2024-05-11 20:02:58 +0700

    Add files via upload

--- src/vga.v
+++ src/vga.v
@@ -1,8 +1,9 @@
-`timescale 1ns / 1ps
+
 
 // 640x480 @60Hz
 module vga(
-	input clk, 			// 100MHz
+	input clk, 		
+	input reset,
 	output HS, VS,
 	output [9:0] x,
 	output [9:0] y,
@@ -12,13 +13,10 @@ module vga(
 	);
 	
 	// Counter Registers, two each for buffering to avoid glitches
-	reg [9:0] xc = 0;
-	reg [9:0] yc = 0;
-	reg [9:0] xc_next = 0;
-	reg [9:0] yc_next = 0;
-	reg [1:0] prescaler = 0;
-	reg HS_reg = 0;
-	reg VS_reg = 0;
+	reg [9:0] xc, yc, xc_next, yc_next;
+	reg [1:0] prescaler;
+	reg HS_reg;
+	reg VS_reg;
 	wire HS_next, VS_next;
 	
 	// Horizontal 640 + fp 16 + HS 96 + bp 48 = 800 pixel clocks
@@ -35,24 +33,34 @@ module vga(
 	assign endframe = ((xc == 0) && (yc == 480) && (prescaler == 0));
 	
 	always @(posedge clk) begin
-		prescaler <= prescaler + 1;
-		if (prescaler == 3) begin		// do every 25Mhz
-			prescaler <= 0;	
-			if (xc == 799) begin
-				xc_next <= 0;
-				yc_next <= yc + 1;
-			end else begin
-				xc_next <= xc + 1;
-			end
-			if (yc == 524) begin
-				yc_next <= 0;
-			end
+		if (reset) begin
+			xc <= 0;
+			yc <= 0;
+			xc_next <= 0;
+			yc_next <= 0;
+			prescaler <= 0;
+			HS_reg <= 0;
+			VS_reg <= 0;
 		end
-		xc <= xc_next;
-		yc <= yc_next;
-		HS_reg <= HS_next;
-		VS_reg <= VS_next;
-
+		else begin
+			prescaler <= prescaler + 1;
+			if (prescaler == 3) begin		// do every 25Mhz
+				prescaler <= 0;	
+				if (xc == 799) begin
+					xc_next <= 0;
+					yc_next <= yc + 1;
+				end else begin
+					xc_next <= xc + 1;
+				end
+				if (yc == 524) begin
+					yc_next <= 0;
+				end
+			end
+			xc <= xc_next;
+			yc <= yc_next;
+			HS_reg <= HS_next;
+			VS_reg <= VS_next;
+		end
 	end
 	
 endmodule
